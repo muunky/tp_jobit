327fd63ade01405ee0db678d63503076fdf324c7
godillon felix <jusdcitron@gmail.com>
2020-11-26 16:21:37 +0100

    <seg

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Repository\CategorieRepository;
+
 use App\Repository\JobsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
